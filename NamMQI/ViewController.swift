2050ba14d8257fb5b12b5a6e2a596a84f6ecc8be
abdusamad <abdusamad2609@gmail.com>
2023-12-13 18:55:35 +0500

    Initial Commit

---
+++ NamMQI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamMQI
+//
+//  Created by Abdusamad Mamasoliyev on 13/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
